9538e48f1735912ca3ae4dfd0c23ebec9a16d9c0
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-05 19:10:22 +0300

    added Header component

---
+++ contact-manager-front/src/Components/Header Component/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+const Header=()=>{
+    return (
+        <div className="nav">    
+         <span>Contact Manager App</span>         
+         </div>
+       );
+
+}
+export default Header;
