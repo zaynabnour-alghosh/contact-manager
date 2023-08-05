77c15259d5ac1cce0d9e9dff90f887b09552e106
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-05 19:17:59 +0300

    edited app component

--- contact-manager-front/src/App.jsx
+++ contact-manager-front/src/App.jsx
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+import Header from "./Components/Header Component/Header";
+import Home from "./Components/Home Component/Home";
+const App=()=>{
+  const contact={
+    "name": "John Doe",
+    "phone": "+ (961) 3 522523",
+    "address_longitude": "54",
+    "address_lattitude": "85"
+    
+}
+   return (
+   <div>      
+      <Home contact={contact}/>
     </div>
   );
 }
