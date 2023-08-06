7478da8cef5fcf4602f4dcc19ff8db3557ffc697
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-08-07 01:13:56 +0300

    adding  leaflet configuration

--- contact-front/src/App.jsx
+++ contact-front/src/App.jsx
@@ -4,15 +4,17 @@ import Main from "./components/Main Component";
 import { BrowserRouter,Routes,Route,createBrowserRouter } from "react-router-dom";
 
 import "./App.css";
+
 const App=()=>{
   return (
     <div className="homex">      
-      <Header />
+      <Header />    
       <BrowserRouter> 
         <Routes>     
           <Route path="/" element={<Main/>}/>       
         </Routes>
       </BrowserRouter> 
+   
     </div>    
   );
 }
--- contact-front/src/components/Main Component/index.jsx
+++ contact-front/src/components/Main Component/index.jsx
@@ -4,12 +4,34 @@ import ContactForm from "../ContactForm Component";
 import ContactContainer from "../ContactContainer Component";
 import { useState,useEffect } from "react";
 import axios from "axios";
+
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+
 const Main=()=>{
     const [contacts, setContacts] = useState([]);
+    let contacts_address=[]
+    const [responseData, setResponseData] = useState(null);
     const fetchContacts = async ()=>{
         const response = await axios.get("http://127.0.0.1:8000/api/contacts");
-        setContacts(response.data)
+        
+        setContacts(response.data);
+        setResponseData(response.data);
+        console.log(response.data.contact_data)
+        
     }
+    useEffect(() => {
+        if (responseData) {
+          const info = responseData.contact_data;
+          console.log(info);
+          for (let i = 0; i < info.length; i++) {
+            const { name, address_longitude, address_latitude } = info[i];
+            contacts_address.push({ name, longitude: address_longitude, latitude: address_latitude });
+          }
+          console.log(contacts_address);
+        }
+      }, [responseData]);
+      
+    
     useEffect(()=>{fetchContacts()}, [])
     return(
         <div>
@@ -20,7 +42,34 @@ const Main=()=>{
                 <div className="ContactSpace">
                     <ContactContainer contacts={contacts} setContacts={setContacts}/>
                 </div> 
-            </div>    
+            </div> 
+            <div id="map">
+        
+            <MapContainer center={[40.7128, -74.006]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {contacts_address.map((contact, index) => (
+                    <Marker position={[parseFloat(contact.latitude),parseFloat( contact.longitude)]} key={index} >
+                    <Popup>
+               contact.name
+                </Popup>
+                    </Marker>
+                ))}
+
+
+
+
+            <Marker position={[40.7128, -74.006]}>
+                <Popup>
+       Center <br /> 
+                </Popup>
+            </Marker>
+            </MapContainer>
+
+            </div>
+               
         </div>     
      );
 }
